ba2dcd515a7e06b3aab28f46b83bcabc54d5a3f9
icantcook <rutvikbade@gmail.com>
2025-05-04 23:34:42 +0530

    remove commented code

--- frontend/src/livekit.jsx
+++ frontend/src/livekit.jsx
@@ -8,8 +8,6 @@ function LiveKitWindow() {
     const identity = 'user_' + Math.floor(Math.random() * 1000);
     const roomName = 'my-room';
     const serverUrl = process.env.REACT_APP_SERVER_URL;
-    // const serverUrl = 'wss://ai-supervisor-6r7louaq.livekit.cloud/';
-    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDYzNjc4ODAsImlzcyI6IkFQSURQYktTNVRYOXJlaCIsIm5iZiI6MTc0NjM2MDY4MCwic3ViIjoicXVpY2tzdGFydCB1c2VyIHVtOWZiciIsInZpZGVvIjp7ImNhblB1Ymxpc2giOnRydWUsImNhblB1Ymxpc2hEYXRhIjp0cnVlLCJjYW5TdWJzY3JpYmUiOnRydWUsInJvb20iOiJxdWlja3N0YXJ0IHJvb20iLCJyb29tSm9pbiI6dHJ1ZX19.NbP6sSjOADz77MJsQqOUIJlq2z0P0CI_rs80Vg4XbLc';
 
     useEffect(() => {
         // Fetch the token from your backend
@@ -24,22 +22,20 @@ function LiveKitWindow() {
     }
 
     return (
-        <div style={{ border: '1px solid #ccc', borderRadius: '8px', padding: '20px', height: '100%', backgroundColor: '#fff' }}>
-            <h4>LiveKit Call Window</h4>
-            <LiveKitRoom
-                serverUrl={serverUrl}
-                token={token}
-                connect={true}
-                video={false}
-                audio={true}
-                style={{ height: '5vh' }}
-            >
-                {/* <MediaDeviceSelect kind="audioinput" /> */}
 
-                <RoomAudioRenderer />
-                <ControlBar />
-            </LiveKitRoom>
-        </div>
+        <LiveKitRoom
+            serverUrl={serverUrl}
+            token={token}
+            connect={true}
+            video={false}
+            audio={true}
+            style={{ height: '5vh' }}
+        >
+            {/* <MediaDeviceSelect kind="audioinput" /> */}
+
+            <RoomAudioRenderer />
+            <ControlBar />
+        </LiveKitRoom>
     );
 
 }
