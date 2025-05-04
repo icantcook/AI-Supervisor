8f57cc582ace4b9e38a52ec05567571291551e8d
icantcook <rutvikbade@gmail.com>
2025-05-04 13:55:13 +0530

    supervisor dashboard

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SupervisorDashboard from './SupervisorDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ padding: '20px', background: '#f0f0f0' }}>Customer Support Dashboard</h1>
+      <SupervisorDashboard />
+    </div>
+  );
+}
+
+export default App;
